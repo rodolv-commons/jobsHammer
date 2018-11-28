1b159a46afca3f29ac5ec70bc3f94544c5bf5d69
Rodolfo Marraffa <rodolfo@atelierdisko.de>
2018-11-28 12:16:29 +0100

    WIP: Add Async data get Work in Progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+import {HttpClient} from "@angular/common/http";
+import 'rxjs/Rx';
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'jobs-hammer';
+
+export class AppComponent implements OnInit {
+  myObs: Observable<Object>;
+  constructor(private http:HttpClient) {
+  }
+
+  ngOnInit() {
+      this.myObs = this.http.get("../assets/data/jobs.json")
+      .pipe(map((res: Response) => res.body));
+      this.myObs.subscribe(jobs => console.log("jobs", jobs));
+  }
+
 }
+
+
+
