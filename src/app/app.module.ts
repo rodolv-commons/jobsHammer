24c6bd282f92c797ed8364f26cad6e22f253fb71
rodolv1979 <rodolfo.marraffa@github.com>
2018-11-29 13:25:58 +0100

    Style jobs component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
-import {HeaderComponent} from './header/header.component';
+import { HeaderComponent} from './header/header.component';
 import { JobsComponent } from './jobs/jobs.component';
 import { JobDetailComponent } from './jobs/job-detail/job-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule, MatGridListModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +21,11 @@ import { JobDetailComponent } from './jobs/job-detail/job-detail.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatGridListModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
